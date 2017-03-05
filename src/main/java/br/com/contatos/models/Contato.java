d5b59653e2d623d1d6abae4eacf7d15157f4c1b9
Luan Oliveira <luanoliveira@localhost.localdomain>
2017-03-05 01:28:22 -0300

    persistence with hibernate

---
+++ src/main/java/br/com/contatos/models/Contato.java
@@ -0,0 +1,54 @@
+package br.com.contatos.models;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="contatos")
+public class Contato {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private Character sexo;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Character getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(Character sexo) {
+		this.sexo = sexo;
+	}	
+	
+}
--- src/main/java/br/com/contatos/tests/ContatoTests.java
+++ src/main/java/br/com/contatos/tests/ContatoTests.java
@@ -1,50 +1,27 @@
-package br.com.contatos.controllers;
+package br.com.contatos.tests;
 
-//import java.util.Calendar;
-
-//import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.caelum.vraptor.Controller;
-import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
-//import br.com.caelum.vraptor.Result;
 import br.com.contatos.models.Contato;
 
-@Controller
-@Path(value="/contatos")
-public class ContatosController {
-	
-	@Get("")
-	public void index() {
-	}
-	
-	@Get("/{id}")
-	public void show(int id) {
-		System.out.println(id);
-	}
-	
-	protected void addContato() {
+public class ContatoTests {
+
+	public static void main(String[] args) {
 		Contato contato = new Contato();
 		contato.setNome("Luan Oliveira");
 		contato.setEmail("luan@java.com");
 		contato.setSexo('m');
-		    
+		System.out.println(contato);
+
 	    EntityManagerFactory factory = Persistence.createEntityManagerFactory("treinamentos");
+
 	    EntityManager manager = factory.createEntityManager();
 	   
 	    manager.getTransaction().begin();    
 	    manager.persist(contato);
 	    manager.getTransaction().commit();
 	}
-	
 
-	
-	@Get("/create")
-	public void create() {
-		this.addContato();
-	}
-	
 }
